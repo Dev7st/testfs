396653ce7f9ad483a300f2e30dc03b02fc856742
bjh <imganchi@gmail.com>
2025-10-27 15:44:46 +0900

    e.stopPropagation

--- test/src/App.js
+++ test/src/App.js
@@ -2,7 +2,9 @@ function App() {
 
 	return (
 	  <div onClick={() => {alert("부모 요소")}}>
-		  <button onClick={() => {alert("Play!")}}>
+		  <button onClick={(e) => {
+			  e.stopPropagation();
+			  alert("Play!")}}>
 			  영화보기
 		  </button>
 		  <button onClick={() => {alert("Upload!")}}>
